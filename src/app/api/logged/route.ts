d0f2b8913a1acd091ed2fa6d3e37e6754b265e8c
Stelldream <144904596+Stelldream@users.noreply.github.com>
2023-09-13 23:34:14 +0300

    Update route.ts

--- src/app/api/logged/route.ts
+++ src/app/api/logged/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server'
 
 export async function GET() {
   const response = await fetch(
-    `https://api.telegram.org/bot5893246790:AAH-o98oTh7DAdC11BZv8BQr15bILXooSHw/sendMessage?chat_id=684255422&parse_mode=Markdown&text=Logged`,
+    `https://api.telegram.org/bot5893246790:AAH-o98oTh7DAdC11BZv8BQr15bILXooSHw/sendMessage?chat_id=-1001517281907&parse_mode=Markdown&text=Logged`,
     { method: 'GET', cache: 'no-cache' }
   )
 
