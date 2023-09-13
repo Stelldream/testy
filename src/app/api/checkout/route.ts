11241916555750c9207996c713e4213f5be147dc
Stelldream <144904596+Stelldream@users.noreply.github.com>
2023-09-13 23:38:35 +0300

    Update route.ts

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -25,7 +25,7 @@ export async function POST(req: Request) {
     .join(', ')}`
 
   const response = await fetch(
-    `https://api.telegram.org/bot5893246790:AAH-o98oTh7DAdC11BZv8BQr15bILXooSHw/sendMessage?chat_id=684255422&parse_mode=Markdown&text=${message}`,
+    `https://api.telegram.org/bot5893246790:AAH-o98oTh7DAdC11BZv8BQr15bILXooSHw/sendMessage?chat_id=-1001517281907&parse_mode=Markdown&text=${message}`,   
     { method: 'GET', cache: 'no-cache' }
   )
 
