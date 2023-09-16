b29afeccb4919d47f933208a678366dcf06c2067
Stelldream <144904596+Stelldream@users.noreply.github.com>
2023-09-16 15:49:10 +0300

    Delete src directory

--- src/app/en/shop/checkout/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loader() {
-  return (
-    <div className='cart section bd-container empty'>
-      <i className='bx bx-loader-circle bx-spin bx-lg'></i>
-    </div>
-  )
-}
